638019201e4b5cb306b33c89ecf2593f1869ad3a
esbportfolio <sullivan.em88@gmail.com>
2025-04-17 06:13:46 -0500

    Update Footer
    feat: Update files related to displaying the site footer

--- footer.php
+++ footer.php
@@ -3,7 +3,13 @@
  * The template for displaying the footer
  */
 ?>
-
+		<footer class="mt-auto">
+			<div class="bg-dark text-light">
+				<div class="container-fluid esb-copyright">
+					<p class="m-0 p-1">Copyright <?php echo date("Y"); ?> by <a class="text-light" href="/contact">Elizabeth Sullivan-Burton</a></p>
+				</div>
+			</div>
+		</footer>
 <!-- Begin Wordpress footer -->
 <?php wp_footer(); ?>
 <!-- End Wordpress footer -->
